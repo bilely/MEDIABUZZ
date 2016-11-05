c159adfca7195f787de45a037ff80ec8c465953b
laurentGIBERT <laurent.gibert@gmail.com>
2016-11-05 18:07:07 +0100

    cocucou

--- src/com/humanbooster/mediabuzz/business/Commentaire.java
+++ src/com/humanbooster/mediabuzz/business/Commentaire.java
@@ -3,5 +3,6 @@ package com.humanbooster.mediabuzz.business;
 import java.util.Date;
 
 public class Commentaire extends Publication{
-	
+		private String contenu;
+		private Publication publication;
 }
